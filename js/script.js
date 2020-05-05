94dd8b50f6ae01b3b57962f2d522b8dc8237224a
Rene Santana <renesantana10@gmail.com>
2020-05-05 20:20:25 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@
   
   src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js";
 
-   $("#text").append("this is so freaking cool.");
+   
+$(document).ready(function() {
+    $("#text").append("this is so freaking cool.");
+});
